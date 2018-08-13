8cd1ab41d72740a9a4c94906e41afe32c7b821f2
danhab99 <dan.habot@gmail.com>
2018-08-13 19:16:42 -0400

    Wrapped middleware in event emitter

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const cheerio = require('cheerio');
-const request = require('request-promise');
+const request = require('request');
+const EventEmitter = require('events');
 const default_message = 'Service denied, P v. NP solved. Read more <a href="https://www.claymath.org/millennium-problems/p-vs-np-problem">here</a>';
 const default_code = 500;
 
-module.export = function(message, code) {
-    return function(req, res, next) {
-        request("https://www.claymath.org/millennium-problems/p-vs-np-problem").then(html => {
-            var $ = cheerio.load(html);
+class PvNP extends EventEmitter {
+    middleware(message, code) {
+        return (req, res, next) => {
+            request("https://www.claymath.org/millennium-problems/p-vs-np-problem", (err, stat, html) => {
+                var $ = cheerio.load(html);
 
-            const results = $('#node-problem-112 > div.field-collection-container.clearfix > div > div > div > div > div > div > div.field-items > div').text();
+                const results = $('#node-problem-112 > div.field-collection-container.clearfix > div > div > div > div > div > div > div.field-items > div').text();
 
-            if (results === "Solved") {
-                res.status(code || default_code).send(message || default_message);
-            } else {
-                next();
-            }
-        });
+                if (results === "Solved") {
+                    res.status(code || default_code).send(message || default_message);
+                    this.emit('solved');
+                } else {
+                    next();
+                }
+            });
+        }
     }
-
 }
+
+module.exports = new PvNP();
